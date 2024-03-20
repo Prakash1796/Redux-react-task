b36575095016833d01090fafa32e8a0671cf5950
Prakash1796 <edgeprakash1796@gmail.com>
2024-03-20 12:41:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 
 
-import { useState } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import Header from "./Components/Header";
-import Dashboard from "./Components/Dashboard";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Create from "./Components/Create";
-import Footer from "./Footer";
-import NavBar from "./Components/NavBars";
-import Home from "./Components/Home";
-import Details from "./Components/Details";
-import Edit from "./Components/Edit";
+import logo from './logo.svg';
+import './App.css';
+import Cart from './component/Cart/Cart';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-[#0fb9b1]">
-        <NavBar />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/dashboard/details/:id" element={<Details />} />
-            <Route path="/dashboard/:id/edit" element={<Edit />} />
-            <Route path="/create" element={<Create />} />
-            
-          </Routes>
-          </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-    </>
+    <div className="App">
+
+<Cart />
+
+    </div>
   );
 }
 
